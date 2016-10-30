238359861b509d425b6a2510c4a66afba7f37b49
CWD <cwd@cloudspokes.com>
2016-10-30 15:39:28 -0400

    trying out this module

--- index.js
+++ index.js
@@ -2,46 +2,27 @@
 
 var async = require('async');
 var config = require('config');
-var gpio = require('pi-gpio');
+var usonic = require('mmm-usonic');
 
-/*
-gpio.open(config.PIN_LED02, 'output', function(err) { // Open pin for output
-    gpio.write(config.PIN_LED02, 1, function() { // Set pin high (1)
-        gpio.close(config.PIN_LED02); // Close pin
-    });
-});
-*/
-
-async.parallel([
-    function(cb) {
-        console.log('opening Trigger for output on ' + config.PIN_SR04_TRIGGER);
-        gpio.open(config.PIN_SR04_TRIGGER, 'output', function(err) {
-            console.log('returned from opening Trigger.', err);
-            cb(err);
-        });
-    },
-    function(cb) {
-        console.log('opening Echo for input on ' + config.PIN_SR04_ECHO);
-        gpio.open(config.PIN_SR04_ECHO, 'input', function(err) {
-            console.log('returned from opening Echo.', err);
-            cb(err);
-        });
-    },
-], function(err, results) {
-    if (err) {
-        console.log(err);
-    } else {
-        gpio.write(config.PIN_SR04_TRIGGER, 0, function() {
-            setTimeout(function() {
-                gpio.write(config.PIN_SR04_TRIGGER, 1, function() {
-                    gpio.close(config.PIN_SR04_TRIGGER);
-                });
-            }, 0.001)
-        });
-    }
-});
 //read SR04
 //check distance
 //onThreshold
 //  fire pins
 //  play audio
+
+usonic.init(function(err) {
+    console.log('initializing sensor');
+
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('creating sensor ');
+        let sensor = usonic.createSensor(config.PIN_SR04_ECHO, config.PIN_SR04_TRIGGER, config.SR04_TIMEOUT);
+
+        setInterval(function(sensor) {
+            let distance = sensor();
+            console.log('distance: ', distance);
+        }, 1000, sensor);
+
+    }
+});
