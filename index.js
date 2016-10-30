f4826a9a7af23b1d504de7d86b4c03d25640dd1c
CWD <cwd@cloudspokes.com>
2016-10-30 15:32:16 -0400

    trying out straight GPIO

--- index.js
+++ index.js
@@ -7,15 +7,39 @@ var gpio = require('pi-gpio');
 /*
 gpio.open(config.PIN_LED02, 'output', function(err) { // Open pin for output
     gpio.write(config.PIN_LED02, 1, function() { // Set pin high (1)
-        gpio.close(config.PIN_LED02); // Close pin 
+        gpio.close(config.PIN_LED02); // Close pin
     });
 });
 */
 
-gpio.open(config.PIN_SR04_TRIGGER,'output',function(err){
-    
+async.parallel([
+    function(cb) {
+        console.log('opening Trigger for output on ' + config.PIN_SR04_TRIGGER);
+        gpio.open(config.PIN_SR04_TRIGGER, 'output', function(err) {
+            console.log('returned from opening Trigger.', err);
+            cb(err);
+        });
+    },
+    function(cb) {
+        console.log('opening Echo for input on ' + config.PIN_SR04_ECHO);
+        gpio.open(config.PIN_SR04_ECHO, 'input', function(err) {
+            console.log('returned from opening Echo.', err);
+            cb(err);
+        });
+    },
+], function(err, results) {
+    if (err) {
+        console.log(err);
+    } else {
+        gpio.write(config.PIN_SR04_TRIGGER, 0, function() {
+            setTimeout(function() {
+                gpio.write(config.PIN_SR04_TRIGGER, 1, function() {
+                    gpio.close(config.PIN_SR04_TRIGGER);
+                });
+            }, 0.001)
+        });
+    }
 });
-
 //read SR04
 //check distance
 //onThreshold
