287534f80e4abfecaf12b9b3061c8bb8b6b9a717
CWD <cwd@lazybaer.com>
2016-10-30 19:08:53 +0000

    fleshing this out for the SR04

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 'use strict';
 
+var async = require('async');
 var config = require('config');
 var gpio = require('pi-gpio');
 
-
+/*
 gpio.open(config.PIN_LED02, 'output', function(err) { // Open pin for output
     gpio.write(config.PIN_LED02, 1, function() { // Set pin high (1)
         gpio.close(config.PIN_LED02); // Close pin 
     });
 });
+*/
 
+gpio.open(config.PIN_SR04_TRIGGER,'output',function(err){
+    
+});
 
 //read SR04
 //check distance
